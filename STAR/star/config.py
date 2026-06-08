0b6056748bea902c35fd2c2c5db9c164b8a2b8d8
Paula <pas.nl@web.de>
2026-06-08 15:14:04 +0200

    changed config datei

--- STAR/star/config.py
+++ STAR/star/config.py
@@ -5,14 +5,14 @@ import os
 # Paths are relative so they work on all operating systems (Windows, Mac, Linux)
 
 class cfg:
-    base = os.path.dirname(os.path.abspath(__file__))
+    base = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
     # Path to male STAR model
-    path_male_star    = os.path.join(base, '..', '..', 'data', 'body_models', 'star', 'male', 'model.npz')
+    path_male_star    = os.path.join(base, 'data', 'body_models', 'star', 'star_male.npz')
     # Path to female STAR model
-    path_female_star  = os.path.join(base, '..', '..', 'data', 'body_models', 'star', 'female', 'model.npz')
+    path_female_star  = os.path.join(base, 'data', 'body_models', 'star', 'star_female.npz')
     # Path to neutral STAR model
-    path_neutral_star = os.path.join(base, '..', '..', 'data', 'body_models', 'star', 'neutral', 'model.npz')
+    path_neutral_star = os.path.join(base, 'data', 'body_models', 'star', 'star_neutral.npz')
 
 # Create instance so "from config import cfg" works
 cfg = cfg()
