Fix für global rotation slider

main.py
@@ -1861,12 +1861,16 @@ class AppWindow:
     # NEW GLOBAL ROTATION
     def _on_rot_x(self, val):
         bm = self._body_model.selected_text
-        if "global_orient" not in AppWindow.POSE_PARAMS[bm]:
+        # Modelle mit global_orient: SMPL, SMPLX, MANO, FLAME, MHR
+        if "global_orient" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["global_orient"][0, 0, 0] = val
+            if bm == 'MHR':
+                AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 3] = val
+        # Modelle mit pose[0] als Root: SUPR, STAR, ANNY
+        elif "pose" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["pose"][0, 0, 0] = val
+        else:
             return
-        AppWindow.POSE_PARAMS[bm]["global_orient"][0, 0, 0] = val
-        # MHR: auch model_parameters sync (Index 3)
-        if bm == 'MHR':
-            AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 3] = val
         self.load_body_model(
             self._body_model.selected_text,
             gender=self._body_model_gender.selected_text,
@@ -1874,11 +1878,14 @@ class AppWindow:
 
     def _on_rot_y(self, val):
         bm = self._body_model.selected_text
-        if "global_orient" not in AppWindow.POSE_PARAMS[bm]:
+        if "global_orient" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["global_orient"][0, 0, 1] = val
+            if bm == 'MHR':
+                AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 4] = val
+        elif "pose" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["pose"][0, 0, 1] = val
+        else:
             return
-        AppWindow.POSE_PARAMS[bm]["global_orient"][0, 0, 1] = val
-        if bm == 'MHR':
-            AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 4] = val
         self.load_body_model(
             self._body_model.selected_text,
             gender=self._body_model_gender.selected_text,
@@ -1886,11 +1893,14 @@ class AppWindow:
 
     def _on_rot_z(self, val):
         bm = self._body_model.selected_text
-        if "global_orient" not in AppWindow.POSE_PARAMS[bm]:
+        if "global_orient" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["global_orient"][0, 0, 2] = val
+            if bm == 'MHR':
+                AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 5] = val
+        elif "pose" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["pose"][0, 0, 2] = val
+        else:
             return
-        AppWindow.POSE_PARAMS[bm]["global_orient"][0, 0, 2] = val
-        if bm == 'MHR':
-            AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 5] = val
         self.load_body_model(
             self._body_model.selected_text,
             gender=self._body_model_gender.selected_text,
@@ -1898,15 +1908,20 @@ class AppWindow:
 
     def _on_rot_reset(self):
         bm = self._body_model.selected_text
-        if "global_orient" not in AppWindow.POSE_PARAMS[bm]:
+        if "global_orient" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["global_orient"] = torch.zeros_like(
+                AppWindow.POSE_PARAMS[bm]["global_orient"]
+            )
+            if bm == 'MHR':
+                AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 3] = 0.0
+                AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 4] = 0.0
+                AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 5] = 0.0
+        elif "pose" in AppWindow.POSE_PARAMS[bm]:
+            AppWindow.POSE_PARAMS[bm]["pose"][0, 0, 0] = 0.0
+            AppWindow.POSE_PARAMS[bm]["pose"][0, 0, 1] = 0.0
+            AppWindow.POSE_PARAMS[bm]["pose"][0, 0, 2] = 0.0
+        else:
             return
-        AppWindow.POSE_PARAMS[bm]["global_orient"] = torch.zeros_like(
-            AppWindow.POSE_PARAMS[bm]["global_orient"]
-        )
-        if bm == 'MHR':
-            AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 3] = 0.0
-            AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 4] = 0.0
-            AppWindow.POSE_PARAMS['MHR']['model_parameters'][0, 5] = 0.0
         self._rot_x.double_value = 0.0
         self._rot_y.double_value = 0.0
         self._rot_z.double_value = 0.0