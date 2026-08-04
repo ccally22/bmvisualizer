6b18e477c9c36636da6527870b721004d47eb7f9
ccally22 <pas.nl@web.de>
2026-08-04 23:35:31 +0200

    Implemented SUPR_WRAPPER.py
    because it somehow got lost

--- Wrapper/SUPR_WRAPPER.py
+++ Wrapper/SUPR_WRAPPER.py
@@ -1,9 +1,23 @@
 from Wrapper.body_model_wrapper import BodyModelWrapper
-
+import 
 
 class SUPR_WRAPPER(BodyModelWrapper):
     def preload_body_model(self, gender):
-        pass
+        self._model = SUPR(f'data/body_models/supr/supr_{gender}.npy')
+        return self
 
-    def forward(self):
-        pass
+    def forward(self, input_params, shape_params, expression=None):
+        # berechnung der neuen werte
+        for k, v in input_params.items():
+            input_params[k] = v.reshape(1, -1)
+
+        model_output = self._model(
+            betas=shape_params,
+            #expression=self._body_exp_tensor,
+            **input_params,
+        )
+        verts = model_output.vertices[0].detach().numpy()
+        joints = model_output.joints[0].detach().numpy()
+        faces = self._model.faces
+
+        return verts, joints, faces
